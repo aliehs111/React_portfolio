0ccec6e4de4b1b3d19c4387470d96a0bcba28f53
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-10-21 14:05:09 -0400

    change to HashRouter

--- react_portfolio/src/App.jsx
+++ react_portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
 import About from './About';
 import Portfolio from './Portfolio';
 import Contact from './Contact';
